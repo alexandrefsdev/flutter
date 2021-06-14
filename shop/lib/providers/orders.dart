0701e15f0a270bfd77609888aee1fdcd8e4eb8b5
Alexandre Silveira <alexandref_ba@r2ti.com.br>
2021-06-14 10:24:01 -0300

    Finalizando modulo 8

--- shop/lib/providers/orders.dart
+++ shop/lib/providers/orders.dart
@@ -5,16 +5,20 @@ import 'package:shop/models/cart_item.dart';
 import 'package:shop/models/order.dart';
 
 class Orders with ChangeNotifier {
-  List<Order> _orders = [];
+  List<Order> _items = [];
 
-  List<Order> get orders {
-    return [..._orders];
+  List<Order> get items {
+    return [..._items];
+  }
+
+  int get itemsCount {
+    return _items.length;
   }
 
   void addOrder(List<CartItem> products, double total) {
     final combine = (double t, i) => t + (i.price * i.quantity);
     final total = products.fold(0.0, combine);
-    _orders.insert(
+    _items.insert(
       0,
       Order(
         id: Random().nextDouble().toString(),
--- shop/lib/views/orders_screen.dart
+++ shop/lib/views/orders_screen.dart
@@ -1,15 +1,24 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:shop/providers/orders.dart';
 import 'package:shop/widgets/app_drawer.dart';
+import 'package:shop/widgets/order_widget.dart';
 
 class OrdersScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    final Orders orders = Provider.of(context);
     return Scaffold(
       appBar: AppBar(
         title: Text('Meus Pedidos'),
       ),
-      body: Container(),
       drawer: AppDrawer(),
+      body: ListView.builder(
+        itemCount: orders.itemsCount,
+        itemBuilder: (ctx, i) => OrderWidget(
+          orders.items[i],
+        ),
+      ),
     );
   }
 }
